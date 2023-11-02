d60c419bd03e55ea1dfc6b0f33920ea17c08080b
MrKopchick <illakoroluk62@gmail.com>
2023-11-02 19:41:32 +0200

    novie deisviya

---
+++ script.js
@@ -0,0 +1,22 @@
+$.ajax({
+    url: `https://my-json-server.typicode.com/MrKopchick/ShopRepository/products`,
+    dataType: 'json',
+    success: (results) => {
+        results.forEach(element => {
+            $('#container').append(`
+                <div class="product">
+                    <img src="${element.photo_url}" >
+                    <p>${element.name}</p>
+                    <p>Description: ${element.description}</p>
+                    <p>Price: ${element.price}</p>
+                    <a href="#">Seller Profile</a>
+                </div>
+            `)
+            console.log(element);
+        });
+        
+    },
+    error: (err) => {
+        console.log("хз чел, иди поплач")
+    }
+});
